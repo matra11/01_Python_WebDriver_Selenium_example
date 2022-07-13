2634fa081498fec68a511f9dacb99c5fb8d52c75
matra11 <62292928+matra11@users.noreply.github.com>
2022-07-13 10:44:36 +0200

    Update test_color.py

--- Tests/test_color.py
+++ Tests/test_color.py
@@ -2,5 +2,5 @@ def test_one_color():
     from colorama import Fore, Back, Style
     print()
     print(Fore.RED + 'some red text')
-    print(Fore.RED + 'some red text')
-    print(Fore.RED + 'DERE')
+    print(Fore.GREEN + 'some red text')
+    print(Fore.BLUE + 'DERE')
